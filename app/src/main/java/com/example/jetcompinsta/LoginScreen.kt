a4ef08debd93a6329658b7ed5273a51f2c7c2b2a
7yes <100316346+7yes@users.noreply.github.com>
2023-09-01 12:28:53 -0500

    password  y keyboard

--- app/src/main/java/com/example/jetcompinsta/LoginScreen.kt
+++ app/src/main/java/com/example/jetcompinsta/LoginScreen.kt
@@ -14,20 +14,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,6 +40,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -55,32 +63,38 @@ fun LoginScreen() {
 
 @Composable
 fun Footer(modifier: Modifier) {
-Column(modifier = modifier.fillMaxWidth()) {
-    Divider(
-        Modifier
-            .background(Color(0xF9F9F9))
-            .height(1.dp)
-            .fillMaxWidth())
-    Spacer(modifier = Modifier.size(24.dp))
-    SingUp()
-    Spacer(modifier = Modifier.size(16.dp))
-}
+    Column(modifier = modifier.fillMaxWidth()) {
+        Divider(
+            Modifier
+                .background(Color(0xF9F9F9))
+                .height(1.dp)
+                .fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.size(24.dp))
+        SingUp()
+        Spacer(modifier = Modifier.size(16.dp))
+    }
 }
 
 @Composable
-fun SingUp(){
+fun SingUp() {
     Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-        Text(text = stringResource(R.string.don_t_have_an_account),
+        Text(
+            text = stringResource(R.string.don_t_have_an_account),
             color = Color(0xFFB5B5B5),
-            modifier = Modifier.padding(horizontal =  4.dp),
+            modifier = Modifier.padding(horizontal = 4.dp),
             fontSize = 12.sp,
-            fontWeight = FontWeight.Bold )
-        Text(text = stringResource(R.string.sign_up),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(R.string.sign_up),
             color = Color(0xFF4EA8E9),
             fontSize = 12.sp,
-            fontWeight = FontWeight.Bold )
+            fontWeight = FontWeight.Bold
+        )
     }
 }
+
 @Composable
 fun Body(modifier: Modifier) {
     var email by rememberSaveable { mutableStateOf("") }
@@ -174,8 +188,34 @@ fun ForgotPassword(modifier: Modifier) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Password(password: String, onTextChanged: (String) -> Unit) {
+    var passwordVisibility by remember { mutableStateOf(false) }
+
     TextField(
-        value = password, onValueChange = { onTextChanged(it) }, modifier = Modifier.fillMaxWidth()
+        value = password, onValueChange = { onTextChanged(it) }, modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = stringResource(R.string.password)) },
+        maxLines = 1,
+        singleLine = true,
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFFB2B2B2),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        trailingIcon = {
+            val imagenIcon = if (passwordVisibility) {
+                Icons.Filled.MoreVert
+            } else {
+                Icons.Filled.MailOutline
+            }
+            IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+             //   androidx.compose.material3.Icon(painter = Icons.Default.Add, contentDescription = "d")
+            }
+        },
+        visualTransformation = if(passwordVisibility){
+            VisualTransformation.None
+        }else{
+            PasswordVisualTransformation()
+        }
     )
 }
 
@@ -183,7 +223,17 @@ fun Password(password: String, onTextChanged: (String) -> Unit) {
 @Composable
 fun Email(email: String, onTextChanged: (String) -> Unit) {
     TextField(
-        value = email, onValueChange = { onTextChanged(it) }, modifier = Modifier.fillMaxWidth()
+        value = email, onValueChange = { onTextChanged(it) }, modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = stringResource(R.string.email)) },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFFB2B2B2),
+            // para quitar la linea de abajo
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
     )
 }
 
